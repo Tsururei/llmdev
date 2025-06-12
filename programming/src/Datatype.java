9f6ee3ae681d143ef90c6a148d2d3f04e987084b
tsururei <rei.tsurubami@avantcorp.com>
2025-06-12 12:43:38 +0900

    最初のコミット

---
+++ programming/src/Datatype.java
@@ -0,0 +1,8 @@
+public class Datatype {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = -5;
+        int c = 0;
+        System.out.println( a + b ); //5
+    }
+}
